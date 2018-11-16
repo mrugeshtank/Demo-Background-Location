ac5274a842aa9d25bc29bd956cd7470e2623362d
DeathStroke <ezio@DeathStrokes-MacBook-Air.local>
2018-11-16 22:27:44 +0530

    Initial Commit

---
+++ Demo Background Location/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Demo Background Location
+//
+//  Created by DeathStroke on 16/11/18.
+//  Copyright © 2018 DeathStroke. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
